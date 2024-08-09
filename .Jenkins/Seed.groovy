63ec9bf3640608aefa8574b676328cb1f2c6eed6
punnawut bucha <punnawut_bucha@outlook.com>
2024-08-09 22:23:06 +0700

    gitignore

---
+++ .Jenkins/Seed.groovy
@@ -0,0 +1,60 @@
+def folderName = 'member.api'
+def gitUrl = 'http://linuxdev02.nio.ngg.local/dotnet/member.api.git'
+def gitBranch = 'refs/heads/master'
+ 
+folder(folderName)
+job("$folderName/Seed") {
+  logRotator(-1, 10)
+  wrappers {
+    timestamps()
+  }
+ 
+  scm {
+    git {
+      branch(gitBranch)
+      remote {
+        url(gitUrl)
+        credentials('gitlab')
+      }
+    }
+  }
+ 
+  steps {
+    triggers {
+      gitlab {
+        triggerOnPush(true)
+        triggerOnMergeRequest(false)
+        triggerOnAcceptedMergeRequest(false)
+        triggerOnClosedMergeRequest(false)
+        triggerOpenMergeRequestOnPush('never')
+        triggerOnNoteRequest(false)
+        noteRegex('')
+        skipWorkInProgressMergeRequest(false)
+        ciSkip(false)
+        setBuildDescription(false)
+        addNoteOnMergeRequest(false)
+        addCiMessage(false)
+        addVoteOnMergeRequest(false)
+        acceptMergeRequestOnSuccess(false)
+        branchFilterType('RegexBasedFilter')
+        includeBranchesSpec('')
+        excludeBranchesSpec('')
+        targetBranchRegex('master')
+        mergeRequestLabelFilterConfig {
+          include('')
+          exclude('')
+        }
+        secretToken('d9604e4ba8c8bd3140ab99e909aa807d')
+        triggerOnPipelineEvent(false)
+      }
+    }
+ 
+    dsl {
+      external "**/*.groovy"
+    }
+ 
+    publishers {
+        wsCleanup()
+    }
+  }
+}
---
+++ .Jenkins/Staging.groovy
@@ -0,0 +1,38 @@
+def projectName = 'member.api'
+def gitUri = 'http://linuxdev02.nio.ngg.local/dotnet/member.api.git'
+def dockerTag = 'linuxdev02.nio.ngg.local:5000/member.api'
+def dockerComposeFile1 = 'Staging/docker-compose.yml'
+
+folder(projectName)
+folder("$projectName/Staging") {
+    description 'The staging environment is the environment in which the application runs when it is live and being used by end users.'
+}
+
+pipelineJob("$projectName/Staging/Deploy_umcs01n1") {
+    logRotator(-1, 10)
+    definition {
+        parameters {
+            stringParam('Tag', 'latest', 'Docker image tag for deploy.')
+        }
+        cps {
+            sandbox()
+            script("""
+                @Library('jenkins-shared-libraries')_
+
+                def _gitBranch = "refs/tags/\$Tag"
+                def _dockerTag = '$dockerTag'.replaceAll(/:\\w+\$/, '') + ":\$Tag"
+
+                netStaging {
+                    remoteHost = 'umcs01n1.nio.ngg.local'
+                    gitUri = '$gitUri'
+                    gitBranch = _gitBranch
+                    projectName = '$projectName'
+                    dockerTag = _dockerTag
+                    dockerComposeFile = '$dockerComposeFile1'
+                }
+            """)
+        }
+    }
+}
+
+
